74af1782162f705232d3b8467a7a8b2e0a3dbfca
Noman Rauf <nomi589@hotmail.com>
2023-09-30 02:15:36 +0500

    Improve the ratings for popular products

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -1,13 +1,13 @@
 import { star } from "../assets/icons";
 
-function PopularProductCard({ imgURL, name, price }) {
+function PopularProductCard({ imgURL, name, price, rating }) {
   return (
     <div className="flex flex-1 flex-col w-full max-sm:w-full">
       <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
       <div className="mt-8 flex justify-start gap-2.5">
         <img src={star} alt="rating" width={24} height={24} />
         <p className="font-montserrat leading-normal text-slate-gray text-xl">
-          4.5
+          {rating}
         </p>
       </div>
       <h3 className="mt-2 text-xl leading-normal font-semibold font-palanquin">
